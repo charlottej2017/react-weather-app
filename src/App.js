b28969eb06e932313513f24d05aeb438f5cf8a19
charlottej2017 <Charlottejones74@gmail.com>
2022-12-13 13:50:31 -0500

    added github url

--- src/App.js
+++ src/App.js
@@ -2,6 +2,22 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://brilliant-cactus-5a7fb9.netlify.app/" target="_blank">
+          Charlotte Jones
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/charlottej2017/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
-scree;
