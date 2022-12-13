809345f68f57216d79f801abb172a4de78e16027
charlottej2017 <Charlottejones74@gmail.com>
2022-12-13 14:24:50 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,25 +4,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://brilliant-cactus-5a7fb9.netlify.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Charlotte Jones
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/charlottej2017/react-weather-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://brilliant-cactus-5a7fb9.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Charlotte Jones
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/charlottej2017/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
