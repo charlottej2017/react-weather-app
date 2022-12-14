602b0ea767a1b87f1bd77cf42e3d125c4fc55519
charlottej2017 <Charlottejones74@gmail.com>
2022-12-14 16:04:27 -0500

    Added basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -31,7 +31,7 @@ export default function Weather() {
       </ul>
       <div className="row mt-3">
         <div className="col-6">
-          <div className="clearfix">
+          <div className="clearfix d-flex">
             <img
               src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
               alt="Cloudy Icon"
