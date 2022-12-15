da09254efa0784a5025aed67fc3c805db7764037
charlottej2017 <Charlottejones74@gmail.com>
2022-12-15 18:45:28 -0500

    cont. api integration

--- src/Weather.js
+++ src/Weather.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
+export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
   function handleResoponse(response) {
     console.log(response.data);
@@ -74,8 +74,7 @@ export default function Weather() {
     );
   } else {
     const apiKey = "b95f9ececad46adfb2b7b5be4da60099";
-    let city = "New York";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResoponse);
 
     return "loading..";
