fb3f3341789327b220f6b3303bb026697e1027c0
charlottej2017 <Charlottejones74@gmail.com>
2022-12-20 19:32:08 -0500

    update to 5 day forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -16,9 +16,6 @@ export default function WeatherForecast(props) {
     return (
       <div className="WeatherForecast">
         <div className="row">
-          <div className="col">
-            <WeatherForecastDay data={forecast[0]} />
-          </div>
           {forecast.map(function (dailyForecast, index) {
             if (index < 5) {
               return (
